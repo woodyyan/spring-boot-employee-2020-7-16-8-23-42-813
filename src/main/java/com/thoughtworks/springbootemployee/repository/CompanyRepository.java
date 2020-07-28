00a57eecca94bdf46fe6561b04f7ba210385b0ac
Songbai Yan <colorguitar@hotmail.com>
2020-07-28 22:15:01 +0800

    format

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,13 +1,8 @@
 package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Company;
-import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 

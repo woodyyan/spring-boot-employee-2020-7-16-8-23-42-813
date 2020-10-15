5d493f1c8b82c2924fc1fb6a60cbc7d8f6b1a368
Songbai Yan <colorguitar@hotmail.com>
2020-10-15 14:21:43 +0800

    add should_get_all_employees_given_page_number_and_size

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -7,6 +7,9 @@ import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
@@ -22,5 +25,24 @@ class EmployeeServiceTest {
         List<EmployeeResponse> employeeResponses = service.getAll();
 
         Assertions.assertEquals(2, employeeResponses.size());
+    }
+
+    @Test
+    void should_get_all_employees_given_page_number_and_size() {
+        EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
+        EmployeeMapper mapper = Mockito.mock(EmployeeMapper.class);
+        EmployeeService service = new EmployeeService(repository, mapper);
+        int page = 1;
+        int size = 10;
+        List<Employee> employees = asList(new Employee(), new Employee());
+        PageRequest pageable = PageRequest.of(page, size);
+        Page<Employee> expected = new PageImpl<>(employees, pageable, 2);
+        Mockito.when(repository.findAll(pageable)).thenReturn(expected);
+
+        Page<EmployeeResponse> employeeResponses = service.getAll(page, size);
+
+        Assertions.assertEquals(10, employeeResponses.getSize());
+        Assertions.assertEquals(1, employeeResponses.getNumber());
+        Assertions.assertEquals(2, employeeResponses.getContent().size());
     }
 }

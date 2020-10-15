f1ea2006b9f5c0f0e72dec14a9bf150bc09708e6
Songbai Yan <colorguitar@hotmail.com>
2020-10-15 15:01:34 +0800

    add should_create_employee_given_employe_request

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.mapper.EmployeeMapper;
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.model.EmployeeRequest;
 import com.thoughtworks.springbootemployee.model.EmployeeResponse;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
@@ -54,6 +55,21 @@ class EmployeeServiceTest {
         Mockito.when(repository.findById(1)).thenReturn(Optional.of(new Employee(1, "Tom", 18, "Male", 1000)));
 
         EmployeeResponse response = service.get(1);
+
+        Assertions.assertEquals(1, response.getId());
+        Assertions.assertEquals("Tom", response.getName());
+    }
+
+    @Test
+    void should_create_employee_given_employe_request() {
+        EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
+        EmployeeService service = new EmployeeService(repository, new EmployeeMapper());
+
+        Employee savedEmployee = new Employee(1, "Tom", 18, "Male", 1000);
+        Mockito.when(repository.save(Mockito.any(Employee.class))).thenReturn(savedEmployee);
+        EmployeeRequest request = new EmployeeRequest("Tom", 18, "Male", 1000);
+
+        EmployeeResponse response = service.create(request);
 
         Assertions.assertEquals(1, response.getId());
         Assertions.assertEquals("Tom", response.getName());

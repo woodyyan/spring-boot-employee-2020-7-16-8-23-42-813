17c9997d981eed8661b077d980ec06e073b7cc7c
Songbai Yan <colorguitar@hotmail.com>
2020-10-15 15:34:11 +0800

    handle CompanyNotFoundException in advice

--- src/main/java/com/thoughtworks/springbootemployee/advice/GlobalControllerAdvice.java
+++ src/main/java/com/thoughtworks/springbootemployee/advice/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.advice;
 
+import com.thoughtworks.springbootemployee.exception.CompanyNotFoundException;
 import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,5 +12,10 @@ public class GlobalControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({EmployeeNotFoundException.class})
     public void handleNotFound(EmployeeNotFoundException exception) {
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({CompanyNotFoundException.class})
+    public void handleNotFound(CompanyNotFoundException exception) {
     }
 }

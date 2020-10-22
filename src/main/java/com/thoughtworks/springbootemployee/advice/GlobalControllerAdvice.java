55bab34e25bba4fb61c2a7fbb891ed47c717147a
Songbai Yan <colorguitar@hotmail.com>
2020-10-22 23:27:37 +0800

    add ErrorResponse to improve advice

--- src/main/java/com/thoughtworks/springbootemployee/advice/GlobalControllerAdvice.java
+++ src/main/java/com/thoughtworks/springbootemployee/advice/GlobalControllerAdvice.java
@@ -3,19 +3,21 @@ package com.thoughtworks.springbootemployee.advice;
 import com.thoughtworks.springbootemployee.exception.CompanyNotFoundException;
 import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({EmployeeNotFoundException.class})
-    public void handleNotFound(EmployeeNotFoundException exception) {
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ErrorResponse handleNotFound(EmployeeNotFoundException exception) {
+        return new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND.name());
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({CompanyNotFoundException.class})
-    public void handleNotFound(CompanyNotFoundException exception) {
+    @ExceptionHandler(CompanyNotFoundException.class)
+    public ErrorResponse handleNotFound(CompanyNotFoundException exception) {
+        return new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND.name());
     }
 }
